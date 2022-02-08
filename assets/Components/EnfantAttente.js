697412b30ec4aa6cab78ecd6ab042ddcd58ad0fb
Yamingue <yamking01@gmail.com>
2022-02-09 00:05:07 +0100

    mise en marche des minutes

--- assets/Components/EnfantAttente.js
+++ assets/Components/EnfantAttente.js
@@ -1,23 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './parking.css'
 
 
-const EnfantAttente = ({ data })=>{
-    const [minute, setMinute] = useState(11)
+const EnfantAttente = ({ data }) => {
+    const [minute, setMinute] = useState(0)
+    useEffect(function () {
+        let date = new Date(data.updateAt)
+        date = date.getTime();
+        let now = new Date();
+        now = now.getTime()
+        let millice = now - date;
+        let seconde = millice/1000
+        setMinute(Math.round(seconde/60))
+    }, [])
     let color = "text-light"
-    if (minute >=3 && minute <=10) {
+    if (minute >= 3 && minute <= 10) {
         color = "text-info"
     }
-    if (minute >=10) {
+    if (minute >= 10) {
         color = "text-warning"
-    } 
+    }
     return (
         <div className="waiting-body">
-            <img className="waiting-img" src={"/"+data.enfant.photo}/>
+            <img className="waiting-img" src={"/" + data.enfant.photo} />
             <div className='waiting-text'>
-                <div>{data.enfant.nom +" "+data.enfant.prenom}</div>
+                <div>{data.enfant.nom + " " + data.enfant.prenom}</div>
                 <div className={color}>
-                    {minute} mn
+                    {minute} minutes
                 </div>
             </div>
             <button className="waiting-btn">
