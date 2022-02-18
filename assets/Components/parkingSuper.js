73d7778e99c0e973d71827c553c2d4e6cf61608a
Yamingue <yamking01@gmail.com>
2022-02-18 15:16:01 +0100

    ajout de parking only a super

--- assets/Components/parkingSuper.js
+++ assets/Components/parkingSuper.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useQuery } from 'react-query';
 import EnfantAttente from './EnfantAttente';
+import ParkingOnly from './ParkingOnly';
 
 
 const ParkingSuper = (props) => {
@@ -34,7 +35,7 @@ const ParkingSuper = (props) => {
     if (error) return 'An error has occurred: ' + error.message
     return (<>
         {
-            data.map(el => <EnfantAttente data={el} key={el.updateAt} />)
+            data.map(el => <ParkingOnly data={el} key={el.updateAt} />)
         }
     </>);
 }
