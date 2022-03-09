2d40e322d3564e87e8ffd39022b94ea84398d583
Yamingue <yamking01@gmail.com>
2022-03-09 12:53:07 +0100

    mise en place du ready gardienne

--- src/Controller/Api/Gardienne/ApiGardienneNotificationController.php
+++ src/Controller/Api/Gardienne/ApiGardienneNotificationController.php
@@ -4,8 +4,10 @@ namespace App\Controller\Api\Gardienne;
 
 use App\Entity\Enfant;
 use App\Entity\Gardienne;
+use App\Entity\Notification;
 use App\Service\SerializeNotification;
 use App\Repository\NotificationRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -37,12 +39,31 @@ class ApiGardienneNotificationController extends AbstractController
         return $this->json(SerializeNotification::collectionToArray($this->notificationRepository->findSalleParking($salle)));
     }
     #[Route('/api/gardienne/ready/{id}', name: 'api_gardienne_ready')]
-    public function ready(Enfant $e)
+    public function ready(Notification $notification=null, ManagerRegistry $managerRegistry)
     {
         /**@var Gardienne */
         $gardienne = $this->getUser();
-        $salle = $gardienne->getSalles();
+        //dd($notification);
+        $notifications = $gardienne->getSalles()->getNotifications();
+        if($notification!=null && $notifications->contains($notification)){
+            $em = $managerRegistry->getManager();
+            $notification->setIsReady(true);
+            $em->persist($notification);
+            $em->flush();
+            return $this->json([
+                'code'=>200,
+                'message'=>'Done'
+            ],200);
+        }else{
+            return $this->json([
+                'code'=>404,
+                'message'=>'Invalide action'
+            ]);
+        }
 
-        return $this->json(SerializeNotification::collectionToArray($this->notificatonRepository->findSalleParking($salle)));
+       // return $this->json($this->getUser());
+       // $salle = $gardienne->getSalles();
+
+        //return $this->json(SerializeNotification::collectionToArray($this->notificatonRepository->findSalleParking($salle)));
     }
 }
