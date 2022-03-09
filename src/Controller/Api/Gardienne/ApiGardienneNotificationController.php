885687b5314da861baf4b3b5ee06e6f987baa482
Yamingue <yamking01@gmail.com>
2022-03-09 14:31:14 +0100

    mise en place close cote gardienne

--- src/Controller/Api/Gardienne/ApiGardienneNotificationController.php
+++ src/Controller/Api/Gardienne/ApiGardienneNotificationController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/api/gardienne')]
 class ApiGardienneNotificationController extends AbstractController
 {
     private $notificationRepository;
@@ -19,7 +20,7 @@ class ApiGardienneNotificationController extends AbstractController
     {
         $this->notificationRepository = $notificationRepository;
     }
-    #[Route('/api/gardienne/notification', name: 'api_gardienne_notification')]
+    #[Route('/notification', name: 'api_gardienne_notification')]
     public function index()
     {
         /**@var Gardienne */
@@ -29,7 +30,7 @@ class ApiGardienneNotificationController extends AbstractController
         return $this->json(SerializeNotification::collectionToArray($this->notificationRepository->findSalleOnWay($salle)));
     }
 
-    #[Route('/api/gardienne/parking', name: 'api_gardienne_parking')]
+    #[Route('/parking', name: 'api_gardienne_parking')]
     public function inparking()
     {
         /**@var Gardienne */
@@ -38,32 +39,58 @@ class ApiGardienneNotificationController extends AbstractController
 
         return $this->json(SerializeNotification::collectionToArray($this->notificationRepository->findSalleParking($salle)));
     }
-    #[Route('/api/gardienne/ready/{id}', name: 'api_gardienne_ready')]
-    public function ready(Notification $notification=null, ManagerRegistry $managerRegistry)
+    #[Route('/ready/{id}', name: 'api_gardienne_ready')]
+    public function ready(Notification $notification = null, ManagerRegistry $managerRegistry)
     {
         /**@var Gardienne */
         $gardienne = $this->getUser();
         //dd($notification);
         $notifications = $gardienne->getSalles()->getNotifications();
-        if($notification!=null && $notifications->contains($notification)){
+        if ($notification != null && $notifications->contains($notification)) {
             $em = $managerRegistry->getManager();
             $notification->setIsReady(true);
             $em->persist($notification);
             $em->flush();
             return $this->json([
-                'code'=>200,
-                'message'=>'Done'
-            ],200);
-        }else{
+                'code' => 200,
+                'message' => 'Done'
+            ], 200);
+        } else {
             return $this->json([
-                'code'=>404,
-                'message'=>'Invalide action'
+                'code' => 404,
+                'message' => 'Invalide action'
             ]);
         }
 
-       // return $this->json($this->getUser());
-       // $salle = $gardienne->getSalles();
+        // return $this->json($this->getUser());
+        // $salle = $gardienne->getSalles();
 
         //return $this->json(SerializeNotification::collectionToArray($this->notificatonRepository->findSalleParking($salle)));
+    }
+    #[Route('/close/{id}')]
+    function close(Notification $notification = null,ManagerRegistry $managerRegistry)
+    {
+        //dd($notification);
+        /**@var Gardienne */
+        $gardienne = $this->getUser();
+        //dd($notification);
+        $notifications = $gardienne->getSalles()->getNotifications();
+        if ($notification != null && $notifications->contains($notification)) {
+            $em = $managerRegistry->getManager();
+            $notification->setClose(true);
+            $em->persist($notification);
+            $em->flush();
+            return $this->json([
+                'code' => 200,
+                'message' => 'Notification close'
+            ], 200);
+        } else {
+            return $this->json([
+                'code' => 404,
+                'message' => 'Invalide action'
+            ]);
+        }
+
+        dd($notification);
     }
 }
