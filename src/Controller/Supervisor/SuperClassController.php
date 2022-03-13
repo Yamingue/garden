407269d946a623ace5dca0f1a43574d9f711d317
Yamingue <yamking01@gmail.com>
2022-03-13 01:15:14 +0100

    correction action salle

--- src/Controller/Supervisor/SuperClassController.php
+++ src/Controller/Supervisor/SuperClassController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Supervisor;
 
+use App\Entity\Salle;
 use App\Form\SalleType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,14 +15,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  */
 class SuperClassController extends AbstractController
 {
-    /**
-     * @Route("/new", name="new_super_class")
-     */
-    public function index(Request $request): Response
+    private $em;
+
+    public function __construct(ManagerRegistry $man)
     {
-        $from = $this->createForm(SalleType::class);
+        $this->em = $man->getManager();
+    }
+    #[Route('/edite/{id}', name:'edite_super_class')]
+    public function edite(Salle $salle,Request $request): Response
+    {
+        $form = $this->createForm(SalleType::class,$salle);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->persist($salle);
+            $this->em->flush();
+            $this->addFlash('success',$salle->getNom().' was update');
+            return $this->redirectToRoute('super');
+        }
         return $this->render('super_class/index.html.twig', [
-            'form' => $from->createView(),
+            'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/delete/{id}', name:'delete_super_class')]
+    public function delete(Salle $salle=null): Response
+    {
+        if ($salle) {
+         
+            $this->em->remove($salle);
+            $this->em->flush();
+            $this->addFlash('success',$salle->getNom().' deleted');
+        }
+       
+        return $this->redirectToRoute('super');
     }
 }
