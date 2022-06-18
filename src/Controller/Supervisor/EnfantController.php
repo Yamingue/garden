265f9b90649b525552c11d41b38a7312543c3a1b
Yamingue Kingatal <yamking01@gmail.com>
2022-06-18 19:17:32 +0200

    mise en form de enfant super

--- src/Controller/Supervisor/EnfantController.php
+++ src/Controller/Supervisor/EnfantController.php
@@ -3,13 +3,15 @@
 namespace App\Controller\Supervisor;
 
 use App\Entity\Enfant;
+use App\Form\EnfantType;
 use App\Form\EnfantEditType;
 use App\Repository\UserRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
+#[Route('/super/enfant')]
 class EnfantController extends AbstractController
 {
     private $manager;
@@ -20,10 +22,57 @@ class EnfantController extends AbstractController
         $this->manager = $doctrine->getManager();
         $this->userRepo = $user;
     }
+    #[Route('/', name:'index_enfant')]
+    public function index(Request $request)
+    {
+        $ecole = $this->getUser();
 
-    /**
-     * @Route("/super/enfant/delete/{id}", name="delete_enfant")
-     */
+       // ajout d'un enfant
+       $enfant = new Enfant();
+       $enfant->setEcole($ecole);
+       $formEnfant = $this->createForm(EnfantType::class, $enfant);
+       $formEnfant->handleRequest($request);
+       if ($formEnfant->isSubmitted()) {
+           if ($formEnfant->isValid()) {
+               # code...
+               $parents = $formEnfant->get("codeParent")->getData();
+               $photo = $formEnfant->get('photo')->getData();
+               $fileName = uniqid() . '.' . $photo->guessExtension();
+               //dump($fileName);
+               $photo->move('images/enfant', $fileName);
+               $enfant->setPhoto('images/enfant/' . $fileName);
+               $this->manager->persist($enfant);
+
+               if ($parents) {
+                   $parents = explode(',', $parents);
+                   foreach ($parents as $code) {
+                       $parent = $this->userRepo->findOneBy(['code' => $code]);
+                       if ($parent) {
+                           # code...
+                           $parent->addEnfant($enfant);
+                           $this->manager->persist($parent);
+                       } else {
+                           $this->addFlash("error", "code $code not exite");
+                       }
+                   }
+
+                   # code...
+               }
+               $this->manager->flush();
+               $this->addFlash('success', $enfant->getNom() . " Ajouter");
+               return $this->redirectToRoute('index_enfant');
+               //dump($enfant, explode(',', $parents), $parents);
+           } else {
+               $this->addFlash("error", "erreur de formulaire");
+           }
+       }
+
+       return $this->render('super_enfant/index.html.twig',[
+        'enfantForm'=>$formEnfant->createView()
+       ]);
+    }
+
+    #[Route("/delete/{id}", name:'delete_enfant')]
     public function delete(Enfant $enfant = null)
     {
         //Liste des gardienne de l'ecole
@@ -52,9 +101,8 @@ class EnfantController extends AbstractController
         return $this->redirectToRoute("super");
     }
 
-    /**
-     * @Route("/super/enfant/edite/{id}", name="edite_enfant")
-     */
+ 
+    #[Route("/edite/{id}", name:'edite_enfant')]
     public function edite(Enfant $enfant, Request $request)
     {
         # code...
